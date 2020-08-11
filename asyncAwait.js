5b414f1772d96eddff5a951bff0de219c145f243
kevinadhiguna <kevin26798@gmail.com>
2020-08-11 18:42:50 +0700

    build: release

---
+++ asyncAwait.js
@@ -0,0 +1,37 @@
+const players = [
+    { name: 'Cristiano Ronaldo', club: 'Juventus' },
+    { name: 'Lionel Messi', club: 'FC Barcelona' },
+    { name: 'Robert Lewandowski', club: 'Bayern Munich' }
+];
+
+function getPlayers(){
+    setTimeout(() => {
+        players.forEach((players) => {
+            console.log(players.name);
+        });
+    }, 1000);
+}
+
+function addPlayer(player) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            players.push(player);
+
+            const error = false;
+
+            if (!error) {
+                resolve();
+            } else {
+                reject('No player found!');
+            }
+        }, 2000);
+    });
+}
+
+async function footballers(player){
+    await addPlayer({ name: 'Kevin De Bruyne', club: 'Manchester City' });
+    
+    getPlayers();
+}
+
+footballers();
